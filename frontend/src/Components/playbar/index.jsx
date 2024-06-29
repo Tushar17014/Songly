9ee4579b93e6735da63f39a70a70b16b18792f80
Tushar <tushar17014@gmail.com>
2024-06-29 14:56:58 +0530

    Home Page Complete

--- frontend/src/Components/playbar/index.jsx
+++ frontend/src/Components/playbar/index.jsx
@@ -1,46 +1,84 @@
 import './index.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faBackward, faForward, faPlay} from '@fortawesome/free-solid-svg-icons';
+import { faPause, faPlay, faBackwardStep, faForwardStep, faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useRef, useState } from 'react';
+import { Song_Path } from '../../constants/const';
+import Slider from './slider';
+import ControlPanel from './controlPanel';
 const Playbar = () => {
-    
+    const [percentage, setPercentage] = useState(0)
+    const [volume, setVolume] = useState(100)
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [duration, setDuration] = useState(0)
+    const [currentTime, setCurrentTime] = useState(0)
+    const song = Song_Path + "5.mp3";
+    const audioRef = useRef()
+    const audio = audioRef.current
+
+    const onChange = (e) => {
+        audio.currentTime = (audio.duration / 100) * e.target.value
+        setPercentage(e.target.value)
+    }
+
+    const onVolChange = (e) => {
+        audio.volume = e.target.value / 100
+        setVolume(e.target.value);
+    }
+
+    const play = () => {
+        audio.volume = 1;
+
+        if (!isPlaying) {
+            setIsPlaying(true)
+            audio.play()
+        }
+
+        if (isPlaying) {
+            setIsPlaying(false)
+            audio.pause()
+        }
+    }
+
+    const getCurrDuration = (e) => {
+        const percent = ((e.currentTarget.currentTime / e.currentTarget.duration) * 100).toFixed(2)
+        const time = e.currentTarget.currentTime
+
+        setPercentage(+percent)
+        setCurrentTime(time.toFixed(2))
+    }
+
     return (
         <>
             <div className="play-bar">
-                <div className="playback-controls">
-                    <button className="control-button">
-                    <FontAwesomeIcon icon={faBackward} size='xl' />
-                    </button>
-                    <button className="control-button">
-                    <FontAwesomeIcon icon={faPlay} size='xl' />
-                    </button>
-                    <button className="control-button">
-                    <FontAwesomeIcon icon={faForward} size='xl' />
-                    </button>
-                </div>
-                    <input type="range" value="0" min="0" max="100" step="1" id='inputProg'></input>
-                {/* <div className="progress-bar">
-                    <div className="progress" style={{ width: `${100}%` }}></div>
-                </div> */}
-                <div className="track-info">
-                    <div className="track-title">Song Title</div>
-                    <div className="track-artist">Artist Name</div>
-                </div>
-                <div className="volume-controls">
-                    <button className="control-button">
-                        <i className="fa fa-volume-up"></i>
-                    </button>
-                    <input
-                        type="range"
-                        min="0"
-                        max="100"
-                        defaultValue="50"
-                        className="volume-slider"
-                        onChange={(event) => {
-                            const volume = event.target.value;
-                            console.log('Volume changed to:', volume);
-                        }}
-                    />
-                </div>
+                <Slider percentage={percentage} onChange={onChange} />
+
+                <div className="playInfo">
+                    <div className="playback-controls">
+                        <FontAwesomeIcon icon={faBackwardStep} size='1x' className='control-button' style={{"fontSize": "22px"}}/>
+                        
+                        {!isPlaying ? (
+                            <FontAwesomeIcon icon={faPlay} size='2x' onClick={play} className='control-button' />
+                        ) : (
+                            <FontAwesomeIcon icon={faPause} size='2x' onClick={play} className='control-button' />
+                        )}
+                        <FontAwesomeIcon icon={faForwardStep} size='1x' className='control-button' style={{"fontSize": "22px"}}/>
+
+                        <ControlPanel play={play} isPlaying={isPlaying} duration={duration} currentTime={currentTime} />
+                    </div>
+                    <div className="track-info">
+                        <div className="track-title">Song Title</div>
+                    </div>
+                    <div className="volume-controls">
+                        <FontAwesomeIcon icon={faVolumeHigh} size='1x' className='control-button' style={{"fontSize": "22px"}}/>
+                        <Slider percentage={volume} onChange={onVolChange}/>
+                    </div>
+                </div> 
+
+
+                <audio ref={audioRef} src={song} onTimeUpdate={getCurrDuration} onLoadedMetadata={(e) => {
+                    setDuration(e.currentTarget.duration.toFixed(2))
+                }}></audio>
+
             </div>
         </>
     )
--- frontend/src/constants/const.js
+++ frontend/src/constants/const.js
@@ -1,1 +1,2 @@
 export const Image_Path = "/images/"
+export const Song_Path = "/songs/"
